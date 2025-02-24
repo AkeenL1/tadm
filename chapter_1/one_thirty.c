2a7ce4718ee0a2f61045bc2cff7ce9eaa6649103
Akeen Lewis <akeenlewis1@gmail.com>
2025-02-23 19:38:34 -0500

    adding closest pair

--- chapter_1/one_thirty.c
+++ chapter_1/one_thirty.c
@@ -13,11 +13,14 @@
 #include <time.h>
 #include <sys/time.h>
 #include <math.h>
+#include <string.h>
 
 struct point {
   int x;
   int y;
   int visited;
+  struct point *connections;
+  int connections_count;
 };
 
 struct graph {
@@ -28,11 +31,17 @@ struct graph {
 struct graph init_graph(int n) {
   struct graph g = {malloc(n * sizeof(struct point)), n};
   for (int i = 0; i < n; i++) {
-    struct point p = {rand() % 100, rand() % 100, 0};
+    struct point p = {rand() % 1000, rand() % 1000, 0, NULL, 0};
     g.points[i] = p;
 
   }
   return g;
+}
+
+int calculate_distance(struct point *p, struct point *p_j) {
+  int dx = p_j->x - p->x;
+  int dy = p_j->y - p->y;
+  return sqrt(dx * dx + dy * dy);
 }
 
 void nearest_neighbor(struct graph g) {
@@ -55,9 +64,7 @@ void nearest_neighbor(struct graph g) {
     for (int j = 0;j<g.n;j++) {
       if (g_copy.points[j].visited == 0) {
         struct point *p_j = &g_copy.points[j];
-        int dx = p_j->x - p->x;
-        int dy = p_j->y - p->y;
-        int distance = sqrt(dx * dx + dy * dy);
+        int distance = calculate_distance(p, p_j);
         if (distance < min_distance) {
           min_distance = distance;
           min_index = j;
@@ -76,10 +83,53 @@ void nearest_neighbor(struct graph g) {
   printf("Visited %d points\n", count);
 }
 
+void closest_pair(struct graph g) {
+  // let n be number of points in graph
+  int n = g.n;
+  // for i = 1 to n -1 do
+  for (int i = 0; i < n - 1 ;i++) {
+    // d = infinite
+    int d = 1000000000;
+    // for each pair of endpoints (s,t) from distinct vertex chain
+    struct point *sm = NULL;
+    struct point *tm = NULL;
+    for (int j = i ; j < n - 1; j++) {
+      for (int k = j + 1; k < n; k++) {
+        // if distance(s,t) <= d then sm = s, tm = t and d = dist s,t
+        int distinct = 1;
+        if (g.points[j].connections != NULL) {
+          for (int l = 0; l < g.points[j].connections_count; l++) {
+            if (&g.points[k] == &g.points[j].connections[l]) {
+              distinct = 0;
+            }
+          }
+        }
+
+        if (calculate_distance(&g.points[j], &g.points[k]) < d && distinct == 1) {
+          d = calculate_distance(&g.points[j], &g.points[k]);
+          sm = &g.points[j];
+          tm = &g.points[k];
+        }
+      }
+    }
+    if (sm->connections == NULL) {
+      sm -> connections = malloc(sizeof(struct point));
+    }
+    sm -> connections[0] = *tm;
+    sm -> connections_count++;
+    if (tm->connections == NULL) {
+      tm -> connections = malloc(sizeof(struct point));
+    }
+    tm -> connections[0] = *sm;
+    tm -> connections_count++;
+    printf("Connected %d %d and %d %d\n", sm->x, sm->y, tm->x, tm->y);
+  }
+}
+
 int main(void){
   srand(time(NULL));
-  // random number between 5 and 10
-  int random_number = rand() % (10000 - 5000 + 1000) + 5000;
+  // random number between 500 and 1000
+  int random_number = rand() % (1000 - 500 + 100) + 500;
   // init graph of points
   struct graph g;
   g = init_graph(random_number);
@@ -99,15 +149,24 @@ int main(void){
 
   long seconds = end.tv_sec - start.tv_sec;
   long microseocnds = end.tv_usec - start.tv_usec;
-  long milliseconds = seconds * 1000 + microseocnds / 1000;
-  printf("Time: %ld ms\n", milliseconds);
-  // reset time
+  long nnmilliseconds = seconds * 1000 + microseocnds / 1000;
+  // printf("Nearest Neighbor Time: %ld ms\n", nnmilliseconds);
   // start time
+  gettimeofday(&start, NULL);
   // run closest pair on points
+  closest_pair(g);
   // end time
+  gettimeofday(&end, NULL);
   // print time two
+  seconds = end.tv_sec - start.tv_sec;
+  microseocnds = end.tv_usec - start.tv_usec;
+  long cpmilliseconds = seconds * 1000 + microseocnds / 1000;
+  printf("Closest Pair Time: %ld ms\n", cpmilliseconds);
   // compare
+  printf("Nearest Neighbor Time: %ld ms\n", nnmilliseconds);
   // is there a better option
+
+
   // print the random number
   printf("%d\n", random_number);
   return 0;
